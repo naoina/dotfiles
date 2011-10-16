ca968d47dddf73b332a4329b9da71a173f7e5147
Naoya INADA <naoina@kuune.org>
2011-10-17 00:12:07 +0900

    Fix the tmux.sh cannot attach to session when a multiple sessions detached

--- bin/tmux.sh
+++ bin/tmux.sh
@@ -2,6 +2,6 @@
 
 exec >/dev/null 2>&1
 
-SESSION=`tmux ls | fgrep -v attached | fgrep -v "server not found" | cut -d ":" -f 1`
+SESSION=`tmux ls | fgrep -v attached | fgrep -m 1 -v "server not found" | cut -d ":" -f 1`
 
 [ -n "$SESSION" ] && exec tmux -2 a -t $SESSION || exec tmux -2
