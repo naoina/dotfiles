f4bd118452efba8f0cfc6baddb7d806801cd2b2e
Naoya Inada <naoina@kuune.org>
2012-06-28 16:35:46 +0900

    Add .gitkeep and new resources

---
+++ bin/auto-hibernate.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+usage() {
+    cat << EOF
+Usage: `basename $0`
+EOF
+    exit $1
+}
+
+[ "$UID" -ne 0 ] && usage
+
+if [ `acpi -b | cut -d " " -f 4 | tr -d "%,"` -le 5 -a `acpi -a | cut -d " " -f 3` = "off-line" ]; then
+    if [ ! -f "/var/lock/auto-hibernate" ]; then
+        /usr/bin/touch /var/lock/auto-hibernate && /usr/sbin/pm-hibernate
+    fi
+else
+    /bin/rm -f /var/lock/auto-hibernate
+fi
