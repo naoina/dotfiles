209569768a5a7e5ea79260aef9735ae6b3b3d68b
Naoya Inada <naoina@kuune.org>
2013-06-06 13:47:42 +0900

    Change command of hibernate from pm-hibernate to systemctl

--- bin/auto-hibernate.sh
+++ bin/auto-hibernate.sh
@@ -11,7 +11,7 @@ EOF
 
 if [ `acpi -b | cut -d " " -f 4 | tr -d "%,"` -le 5 -a `acpi -a | cut -d " " -f 3` = "off-line" ]; then
     if [ ! -f "/var/lock/auto-hibernate" ]; then
-        /usr/bin/touch /var/lock/auto-hibernate && /usr/sbin/pm-hibernate
+        /usr/bin/touch /var/lock/auto-hibernate && /usr/bin/systemctl hibernate
     fi
 else
     /bin/rm -f /var/lock/auto-hibernate
