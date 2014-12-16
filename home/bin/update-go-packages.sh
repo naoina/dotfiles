6073433f1ef4366275c7946ca2c65233f042bea6
Naoya Inada <naoina@kuune.org>
2014-12-16 22:25:06 +0900

    bin: Update update-go-packages

--- home/bin/update-go-packages.sh
+++ home/bin/update-go-packages.sh
@@ -8,14 +8,17 @@ EOF
 }
 
 gopkgs=(
-    'code.google.com/p/go.tools/cmd/godoc'
+    'golang.org/x/tools/cmd/godoc'
     'github.com/josharian/impl'
     'github.com/motemen/ghq'
     'github.com/lib/pq'
+    'github.com/jackc/pgx'
     'github.com/mattn/go-sqlite3'
     'github.com/go-sql-driver/mysql'
     'github.com/peco/peco/cmd/peco'
     'github.com/peco/migemogrep'
+    'github.com/noraesae/orange-cat/cmd/orange'
+    'golang.org/x/tools/cmd/stringer'
 )
 
 for pkg in ${gopkgs[@]}; do
