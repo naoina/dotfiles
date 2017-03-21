747a0b6d1a2a1f16fefc43ef82466641cd9cb3ae
Naoya Inada <naoina@kuune.org>
2017-03-21 21:46:36 +0900

    ipython: Modify import and setting modules

--- home/.ipython/profile_default/ipython_config.py
+++
@@ -1,6 +0,0 @@
-c = get_config()
-c.InteractiveShellApp.exec_lines = [
-    'import numpy as np',
-    'import pandas as pd',
-    'import matplotlib.pyplot as plt',
-]
---
+++ home/.ipython/profile_default/startup/99-pandas.py
@@ -0,0 +1,3 @@
+import pandas as pd
+
+pd.set_option('display.width', 100)
