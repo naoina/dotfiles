11190508857f08422295ba4ad609b6ca4b7e5c75
Naoya Inada <naoina@kuune.org>
2023-01-02 11:54:38 +0900

    bin/update-go-packages: updated

--- home/bin/update-go-packages.sh
+++ home/bin/update-go-packages.sh
@@ -3,33 +3,29 @@
 set -eu
 
 usage() {
-    cat << EOF
-Usage: `basename $0`
+    cat <<EOF
+Usage: $(basename $0)
 EOF
     exit $1
 }
 
 gopkgs=(
-    # 'github.com/motemen/ghq'
-    'github.com/lib/pq'
-    'github.com/jackc/pgx'
-    'github.com/mattn/go-sqlite3'
-    'github.com/go-sql-driver/mysql'
+    'github.com/x-motemen/ghq'
+    # 'github.com/lib/pq'
+    # 'github.com/jackc/pgx'
+    # 'github.com/mattn/go-sqlite3'
+    # 'github.com/go-sql-driver/mysql'
     'github.com/koron/gomigemo/cmd/gmigemo'
     'golang.org/x/tools/cmd/stringer'
     'github.com/cespare/prettybench'
-    'github.com/gohugoio/hugo'
     'golang.org/x/tools/cmd/benchcmp'
     'golang.org/x/tools/gopls'
     'golang.org/x/perf/cmd/benchstat'
     'github.com/shurcooL/markdownfmt'
     'github.com/syohex/byzanz-window/cmd/byzanz-window'
-    'github.com/dvyukov/go-fuzz'
-    'github.com/dvyukov/go-fuzz-build'
-    'github.com/github/git-lfs'
-    'github.com/golang/dep/cmd/dep'
+    # 'github.com/github/git-lfs'
     'github.com/codegangsta/gin'
-    'github.com/derekparker/delve/cmd/dlv'
+    'github.com/go-delve/delve/cmd/dlv'
     'github.com/cweill/gotests/...'
     'github.com/direnv/direnv'
     'github.com/ckaznocha/protoc-gen-lint'
@@ -38,5 +34,5 @@ gopkgs=(
 )
 
 for pkg in ${gopkgs[@]}; do
-    go get -u -v $pkg
+    go install -v ${pkg}@latest
 done
